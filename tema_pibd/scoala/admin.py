df80fc5704c8778770a24feebc066a4eb71addb5
stefan.damian369 <stefan.damian369@gmail.com>
2021-01-19 17:28:34 +0200

    Add files via upload

---
+++ tema_pibd/scoala/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Elev, Profesor, Disciplina, Aranjament
+
+admin.site.register(Elev)
+admin.site.register(Profesor)
+admin.site.register(Disciplina)
+admin.site.register(Aranjament)
